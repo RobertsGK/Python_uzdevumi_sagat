8409ab8a012f1d050c2584246f87b2532ab75939
RobertsGK <148037449+RobertsGK@users.noreply.github.com>
2023-10-30 10:28:19 +0200

    Update 7uzd.py

--- 7uzd.py
+++ 7uzd.py
@@ -2,7 +2,26 @@
 Papildināt programmu ar ciklu, kurā sarakstā esošiem uzvārdiem tiktu 
 pievienots klāt doktora nosaukums - Dr.
 """
-
+"""
 
 saraksts1=["Kalniņš", "Opmanis", "Vēzis", "Almane"]
 sarakts2=[]
+
+for uzvards in saraksts1:
+    doktors="Dr. "+uzvards
+    saraksts2. append(doktors)
+
+print(saraksts2)
+
+
+"""
+
+
+#2.variants
+saraksts1=["Kalniņš", "Opmanis", "Vēzis", "Almane"]
+saraksts2=[]
+def pievienot_dok(uzvards):
+    return "Dr.  "+uzvards
+
+saraksts2=list(map(pievienot_dok, saraksts1))
+print(saraksts2)
